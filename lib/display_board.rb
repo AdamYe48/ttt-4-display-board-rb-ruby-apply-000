a4a84aabb20d5d445684bb63acf61a7722ebd080
Adam Ye <adamye48@gmail.com>
2020-08-11 22:25:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,9 +13,5 @@ end
 puts board = ["    ", "    ", "    ", "    ", " X ", "     ", "    ", "   ", "   "]
 
 def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
+  
 end
